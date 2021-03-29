6469a2c5177c7797d7fc77449378c8dbf4d612bb
RIDE SHARER <kanna.raki998@gmail.com>
2021-03-29 12:01:05 +0530

    PUBLISHED VERSION

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
 import { EnquiryFormComponent } from './enquiry-form/enquiry-form.component';
 import {HttpClientModule} from '@angular/common/http';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule} from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
@@ -20,14 +15,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     EnquiryFormComponent
   ],
   imports: [
-    NgbModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
     ReactiveFormsModule
   ],
   providers: [],
